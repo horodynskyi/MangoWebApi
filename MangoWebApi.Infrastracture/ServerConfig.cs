06d0169026afa819f9870d5f17c6eeb4b9ae4762
horodynskyi <horodynskyi@users.noreply.github.com>
2021-03-09 02:18:24 +0200

    Add

---
+++ MangoWebApi.Infrastracture/ServerConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoWebApi.Infrastracture
+{
+    public class ServerConfig
+    {
+        public MongoDBConfig MongoDB { get; set; } = new MongoDBConfig();
+    }
+}
